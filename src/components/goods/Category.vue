d58f002527589c68a39aa9334127ff6e3f20b410
Hurst <304090717@qq.com>
2021-03-08 15:22:14 +0800

    Goods Category - Added pagination area

--- src/components/goods/Category.vue
+++ src/components/goods/Category.vue
@@ -18,16 +18,26 @@
             <!-- table  -->
             <tree-table :data="goodsList" :columns="columns" :selection-type="false" :expand-type="false" show-index
                 index-text="#" border :show-row-hover="false">
-               <template v-slot:isok="scope">
-                   <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
-                   <i class="el-icon-error" v-else style="color: lightgreen;"></i>
-               </template>
+                <template v-slot:isok="scope">
+                    <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
+                    <i class="el-icon-error" v-else style="color: red;"></i>
+                </template>
+                <template v-slot:order="scope">
+                    <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+                    <el-tag type="success" size="mini" v-else-if="scope.row.cat_level === 1">二级</el-tag>
+                    <el-tag type="warning" size="mini" v-else>三级</el-tag>
+                </template>
+                <template v-slot:operate="scope">
+                    <el-button type="primary" icon="el-icon-edit" size="mini">Edit</el-button>
+                    <el-button type="danger" icon="el-icon-delete" size="mini">Delete</el-button>
+                </template>
             </tree-table>
 
             <!-- pagination area -->
-            <!-- <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
-                :current-page.sync="currentPage3" :page-size="100" layout="prev, pager, next, jumper" :total="total">
-            </el-pagination> -->
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                :current-page.sync="queryInfo.pagenum" :page-sizes="[3,5,10,15]" :page-size="queryInfo.pagesize"
+                layout="total, sizes, prev, pager, next, jumper" :total="total">
+            </el-pagination>
         </el-card>
     </div>
 </template>
@@ -52,6 +62,16 @@
                         label: 'IsValid',
                         type: 'template',
                         template: 'isok'
+                    },
+                    {
+                        label: 'Sorting',
+                        type: 'template',
+                        template: 'order'
+                    },
+                    {
+                        label: 'Operation',
+                        type: 'template',
+                        template: 'operate'
                     }
                 ]
             }
@@ -70,6 +90,16 @@
                 this.goodsList = res.data.result
                 this.total = res.data.total
                 console.log(this.goodsList)
+            },
+            // Monitor the change of pagesize -- 每页显示几条数据
+            handleSizeChange(newSize) {
+                this.queryInfo.pagesize = newSize
+                this.getGoodsList()
+            },
+            // Monitor the change of pagenum -- 页码的改变
+            handleCurrentChange(newPage) {
+                this.queryInfo.pagenum = newPage
+                this.getGoodsList()
             }
         }
 
