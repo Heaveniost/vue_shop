8d53001b7f4abf4fa81d4277fa5378da6a00ff95
Hurst <304090717@qq.com>
2021-03-10 09:00:09 +0800

    Goods Category - Completed the function of adding goods

--- src/components/goods/Category.vue
+++ src/components/goods/Category.vue
@@ -11,13 +11,13 @@
             <!-- add category button -->
             <el-row>
                 <el-col>
-                    <el-button type="primary" @click="addCateDialogVisible = true">Add Category</el-button>
+                    <el-button type="primary" @click="showAddCateDialog">Add Category</el-button>
                 </el-col>
             </el-row>
 
             <!-- table  -->
-            <tree-table :data="goodsList" :columns="columns" :selection-type="false" :expand-type="false" show-index
-                index-text="#" border :show-row-hover="false">
+            <tree-table class="tree-table" :data="goodsList" :columns="columns" :selection-type="false"
+                :expand-type="false" show-index index-text="#" border :show-row-hover="false">
                 <template v-slot:isok="scope">
                     <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
                     <i class="el-icon-error" v-else style="color: red;"></i>
@@ -39,6 +39,24 @@
                 layout="total, sizes, prev, pager, next, jumper" :total="total">
             </el-pagination>
         </el-card>
+
+        <!-- add category dialog -->
+        <el-dialog title="Add Category" :visible.sync="addCateDialogVisible" width="50%" @close="addCateDialogClosed">
+            <el-form :model="addCateForm" :rules="addCateFormRules" ref="addCateRef" label-width="150px">
+                <el-form-item label="Category name: " prop="cat_name">
+                    <el-input v-model="addCateForm.cat_name"></el-input>
+                </el-form-item>
+                <el-form-item label="Parent Category: ">
+                    <!-- options: data origin -->
+                    <el-cascader v-model="selectedKeys" :options="parentCateList" :props="cascaderProps"
+                        @change="parentCateChange" clearable></el-cascader>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="addCateDialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="addCategory">Confirm</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -73,7 +91,28 @@
                         type: 'template',
                         template: 'operate'
                     }
-                ]
+                ],
+                addCateForm: {
+                    cat_name: '', // category name 
+                    cat_pid: 0, // parent id
+                    cat_level: 0
+                },
+                addCateFormRules: {
+                    cat_name: [{
+                        required: true,
+                        message: 'Please input Category name',
+                        trigger: 'blur'
+                    }]
+                },
+                parentCateList: [], // for add dialog
+                cascaderProps: {
+                    expandTrigger: 'hover',
+                    value: 'cat_id',
+                    label: 'cat_name',
+                    children: 'children',
+                    checkStrictly: true
+                },
+                selectedKeys: [], // selected parent keys 
             }
         },
         created() {
@@ -100,12 +139,62 @@
             handleCurrentChange(newPage) {
                 this.queryInfo.pagenum = newPage
                 this.getGoodsList()
+            },
+            // show and hide add category dialog
+            showAddCateDialog() {
+                this.addCateDialogVisible = true
+                this.getParentCateList()
+            },
+            async getParentCateList() {
+                const {
+                    data: res
+                } = await this.$http.get('categories', {
+                    type: 2
+                })
+                if (res.meta.status !== 200) return this.$message.error('Failed to access goods info')
+                this.parentCateList = res.data
+                console.log(res)
+            },
+            // selected option changed
+            parentCateChange() {
+                // console.log(this.selectedKeys)
+                const l = this.selectedKeys.length
+                if (l > 0) {
+                    this.addCateForm.cat_pid = this.selectedKeys[l - 1]
+                    this.addCateForm.cat_level = l
+                    return
+                } else {
+                    this.addCateForm.cat_pid = 0
+                    this.addCateForm.cat_level = 0
+                }
+            },
+            addCategory() {
+                this.$refs.addCateRef.validate(async valid => {
+                    if(!valid) return 
+                    const {data:res} = await this.$http.post('categories', this.addCateForm)
+                    if(res.meta.status !== 201) return this.$message.error('Failed to add cate')
+                    this.$message.success('Succeed to add cate')
+                    this.getGoodsList()
+                    this.addCateDialogVisible = false 
+                })
+            },
+            // when dialog closed, reset data
+            addCateDialogClosed() {
+                this.$refs.addCateRef.resetFields()
+                this.selectedKeys = []
+                this.addCateForm.cat_pid = 0
+                this.addCateForm.cat_level = 0
             }
         }
-
     }
 </script>
 
 <style lang="less" scoped>
+    .tree-table {
+        margin-top: 15px;
+    }
 
+    .el-cascader {
+        width: 100%;
+    }
 </style>>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,7 @@ import {
     Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, 
     Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card,
     Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox,
-    Tag, Tree, Select, Option
+    Tag, Tree, Select, Option, Cascader
 } from 'element-ui'
 import { options } from 'less'
 
@@ -34,5 +34,6 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
